b4c102f34bd274fb634e4c5c3e1c0a0e580fa0a6
Michael Steinle <michael.steinle@gmail.com>
2023-12-03 11:44:19 +0100

    day03 done

--- day03/Program.cs
+++ day03/Program.cs
@@ -49,6 +49,7 @@ for (int i = 0; i < size; i++){
 
 Console.WriteLine(sum);
 
+//this will horrifically fail if next to one * there is the same number twice.
 int gear = 0;
 for (int i = 0; i < size; i++){
     for (int j = 0; j < size; j++){
