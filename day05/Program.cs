f333b1eddf6ca0fa7eabb06c96c75beb18ce2d79
Michael Steinle <michael.steinle@gmail.com>
2023-12-05 21:58:51 +0100

    day05 done, finally

--- day05/Program.cs
+++ day05/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Net.Sockets;
-
-string file = File.ReadAllText("./day05/input.txt");
+﻿string file = File.ReadAllText("./day05/input.txt");
 string[] sections = file.Split("\n\n");
 List<Mapping>[] maps= new List<Mapping>[sections.Length-1];
 for (int i = 0; i < maps.Length; i++){
@@ -21,18 +19,12 @@ for (int i = 1; i < sections.Length; i++){
         maps[i-1].Add(r);
     }
     maps[i-1].Sort();
-    foreach (Mapping item in maps[i-1]){
-        Console.Write(item);
-        Console.Write(", ");
-    }
-    Console.WriteLine();
 }
 
 long lowest = long.MaxValue;
 
 for (int i = 0; i < seed.Length; i++){
     long v = seed[i];
-    Console.WriteLine("----SEED:" + v);
     for (int j = 0; j < maps.Length; j++){
         long v1 =v;
         foreach (Mapping range in maps[j]){
@@ -42,7 +34,6 @@ for (int i = 0; i < seed.Length; i++){
                 break;
             }
         }
-        Console.WriteLine(j + " from " + v + " to " + v1);
         v = v1;
     }
     if (v < lowest){
@@ -52,11 +43,45 @@ for (int i = 0; i < seed.Length; i++){
 Console.WriteLine(lowest);
 
 
-class Range(long start, long span, int type){
+List<Range> seeds = [];
+for (int i = 0; i < seed.Length; i+=2){
+    seeds.Add(new(seed[i], seed[i+1]));
+}
 
-    private long Start {get;} = start;
-    private long Span {get;} = span;
-    private long Type {get;} = type; 
+for (int i = 0; i < maps.Length; i++){
+    List<Range> done = [];
+    foreach(Mapping m in maps[i]){
+        List<Range> ss = [];
+        foreach (Range sd in seeds){
+            Range[] split = m.Split(sd);
+            if(split[1] is not null){
+                done.Add(split[1]);
+            }
+            if(split[0] is not null){
+                ss.Add(split[0]);
+            }
+            if(split[2] is not null){
+                ss.Add(split[2]);
+            }
+        }
+        seeds = ss;
+    }
+    done.AddRange(seeds);
+    seeds = done;
+}
+
+Range low = new(long.MaxValue,1);
+foreach (Range ss in seeds){
+    if(ss.Start < low.Start){
+        low = ss;
+    }
+}
+
+Console.WriteLine(low.Start);
+
+class Range(long start, long span){
+    public long Start {get;} = start;
+    public long Span {get;} = span;
 }
 
 
@@ -83,8 +108,28 @@ class Mapping(long dest, long source, long ranges): IComparable<Mapping>{
         }
     }
 
-    public override string ToString(){
-        return "["+Dest+ ", " + Source + ", " + Ranges+"]";
+    public Range[] Split(Range r){
+        Range[] rs = new Range[3];
+        if(r.Start < this.Source){
+            long end = Math.Min(r.Start+r.Span, this.Source-1);
+            if(end - r.Start >= 0){
+                rs[0] = new(r.Start, end - r.Start+1);
+            }
+        }
+        if(r.Start+r.Span-1 >= this.Source && r.Start <= this.Source+this.Ranges-1){
+            long start = Math.Max(this.Source, r.Start);
+            long end = Math.Min(r.Start+r.Span-1, this.Source+this.Ranges-1);
+            if(end - start >= 0){
+                rs[1] = new(this.Map(start), end - start+1);
+            }
+        }
+        if(r.Start+r.Span-1 >= this.Source+this.Ranges-1){
+            long start = Math.Max(this.Source+this.Ranges, r.Start);
+            long end = r.Start+r.Span-1;
+            if(end - start >= 0){
+                rs[2] = new(start, end-start+1);
+            } 
+        }
+        return rs;
     }
-
 }
