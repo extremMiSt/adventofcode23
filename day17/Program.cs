5f0f453ab83137020f0786820db9f621cdc931ab
Michael Steinle <michael.steinle@gmail.com>
2023-12-17 13:02:13 +0100

    push to different device

---
+++ day17/Program.cs
@@ -0,0 +1,18 @@
+﻿StreamReader reader = new(File.OpenRead("./day17/example1.txt")); 
+
+List<String> map = [];
+
+int mX = 0;
+int mY = 0;
+while(!reader.EndOfStream){
+    string? s = reader.ReadLine();
+    if(s is not null){
+        mX = s.Length;
+        map.Add(s);
+    }
+    mY++;
+}
+
+List<Tuple<int,int>> done = [];
+PriorityQueue<Tuple<List<Tuple<int,int>>,char>, int> front = new();
+front.Enqueue(new([new(0,0)],'-'), 0);
