b74531415caa6f14a47aae6cadfe3f1009cd02d4
Michael Steinle <michael.steinle@gmail.com>
2023-12-18 10:55:04 +0100

    day18 done

--- day18/Program.cs
+++ day18/Program.cs
@@ -1,58 +1,83 @@
-﻿StreamReader reader = new(File.OpenRead("./day18/input.txt")); 
+﻿using System.Numerics;
 
-HashSet<Tuple<int,int>> trench = [];
+StreamReader reader = new(File.OpenRead("./day18/input.txt")); 
 
-int curX = 1;
-int curY = 1;
+int cTrench1 = 0;
+List<Tuple<int,int>> corners1 = [];
+int curX1= 1;
+int curY1 = 1;
 
-int minX = curX;
-int maxX = curX;
-int minY = curY;
-int maxY = curY;
+int cTrench2 = 0;
+List<Tuple<int,int>> corners2 = [];
+int curX2= 1;
+int curY2 = 1;
 
 while(!reader.EndOfStream){
     string? s = reader.ReadLine();
     if(s is not null){
         string[] split = s.Split(" ");
-        string dir = split[0];
-        int dist = int.Parse(split[1]);
-        if(dir=="U"){
-            for (int i = 1; i <= dist; i++){
-                trench.Add(new(curX,curY-i));
-            }
-            curY = curY-dist;
-            if(curY < minY) minY = curY;
-        }else if(dir=="D"){
-            for (int i = 1; i <= dist; i++){
-                trench.Add(new(curX,curY+i));
-            }
-            curY = curY+dist;
-            if(curY > maxY) maxY = curY;
-        }else if(dir=="L"){
-            for (int i = 1; i <= dist; i++){
-                trench.Add(new(curX-i,curY));
-            }
-            curX = curX-dist;
-            if(curX < minX) minX = curX;
-        }else if(dir=="R"){
-            for (int i = 1; i <= dist; i++){
-                trench.Add(new(curX+i,curY));
-            }
-            curX = curX+dist;
-            if(curX > maxX) maxX = curX;
+        string dir1 = split[0];
+        int dist1 = int.Parse(split[1]);
+        string color = split[2];
+        int dist2 = int.Parse(color[2..7],System.Globalization.NumberStyles.HexNumber);
+        string dir2 = color[7..8];
+        if(dir1=="U"){
+            cTrench1 += dist1;
+            curY1 = curY1-dist1;
+            corners1.Add(new(curX1,curY1));
+        }else if(dir1 == "D"){
+            cTrench1 += dist1;
+            curY1 = curY1+dist1;
+            corners1.Add(new(curX1,curY1));
+        }else if(dir1 == "L"){
+            cTrench1 += dist1;
+            curX1 = curX1-dist1;
+            corners1.Add(new(curX1,curY1));
+        }else if(dir1 == "R"){
+            cTrench1 += dist1;
+            curX1 = curX1+dist1;
+            corners1.Add(new(curX1,curY1));
+        }else{
+            throw new("nope");
+        }
+
+        if(dir2=="3"){
+            cTrench2 += dist2;
+            curY2 = curY2-dist2;
+            corners2.Add(new(curX2,curY2));
+        }else if(dir2=="1"){
+            cTrench2 += dist2;
+            curY2 = curY2+dist2;
+            corners2.Add(new(curX2,curY2));
+        }else if(dir2=="2"){
+            cTrench2 += dist2;
+            curX2 = curX2-dist2;
+            corners2.Add(new(curX2,curY2));
+        }else if(dir2=="0"){
+            cTrench2 += dist2;
+            curX2 = curX2+dist2;
+            corners2.Add(new(curX2,curY2));
         }else{
             throw new("nope");
         }
     }
 }
+corners1.Add(corners1.First());
+corners2.Add(corners2.First());
 
-for(int y = minY; y <= maxY; y++){
-    for(int x = minX; x <= maxX; x++){
-        if(trench.Contains(new(x,y))){
-            Console.Write('#');
-        }else{
-            Console.Write('.');
-        }
+Console.WriteLine("part 1: " + interior(corners1, cTrench1));
+Console.WriteLine("part 2: " + interior(corners2, cTrench2));
+
+static long interior(List<Tuple<int,int>> corners, long cTrench){
+    BigInteger sum = BigInteger.Zero;
+    for(int i = 0; i < corners.Count-1; i++){
+        BigInteger b1 = BigInteger.Add(corners[i].Item2, corners[i+1].Item2);
+        BigInteger b2 = BigInteger.Subtract(corners[i].Item1, corners[i+1].Item1);
+        BigInteger b3 = BigInteger.Multiply(b1,b2);
+        sum = BigInteger.Add(sum, b3);
     }
-    Console.WriteLine();
+    sum = BigInteger.Abs(sum);
+    sum = BigInteger.Divide(sum, 2);
+    long pick = ((long)sum) - cTrench/2 + 1;
+    return pick + cTrench;
 }
