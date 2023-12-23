634d00cdd9d32eedd5f8f27f2c7ff92b87bbfe5a
extremMiSt <michael.steinle@googlemail.com>
2023-12-23 07:54:00 +0100

    added tasks, day23 stub

---
+++ day23/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Immutable;
+using Point = System.Tuple<int, int>;
+
+StreamReader reader = new(File.OpenRead("./day23/example1.txt")); 
+
+List<string> map = [];
+
+int line = 0;
+int width = 0;
+while(!reader.EndOfStream){
+    string? s = reader.ReadLine();
+    if(s is not null){
+        map.Add(s);
+        width = s.Length;
+    }
+    line++;
+}
+
+Stack<ImmutableList<Point>> q = new();
+ImmutableList<Point> start = ImmutableList.Create<Point>([new(0,1)]);
+q.Push(start);
+
+while(q.Count != 0){
+    
+}
